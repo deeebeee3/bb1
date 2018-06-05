a9ddcdc9fe58686887578df293a818eed3841d4e
Deepak Bhari <deepakbhari@Deepaks-MBP-2.home>
2018-06-05 13:16:49 +0100

    A form for creating new records - adding an edit view

---
+++ Modules/extensions/UserPreferences@1.0.0/JavaScript/UserPreferences.Edit.View.js
@@ -0,0 +1,50 @@
+define('UserPreferences.Edit.View'
+, [
+    'Backbone'
+  , 'Backbone.FormView'
+  , 'underscore'
+  , 'user_preferences_edit.tpl'
+  ]
+, function
+  (
+    Backbone
+  , FormView
+  , _
+  , user_preferences_edit_tpl
+  )
+{
+  'use strict';
+
+  return Backbone.View.extend({
+    template: user_preferences_edit_tpl
+
+  , events:
+    {
+      'submit form': 'saveForm'
+    }
+
+  , initialize: function (options)
+    {
+      this.model = options.model;
+      this.application = options.application;
+      FormView.add(this);
+    }
+
+  , typeOptions: function ()
+    {
+      var options = [
+        {internalid: 1, name: _.translate('Color'), isSelected: (this.model.get('type') == 'Color')}
+      , {internalid: 2, name: _.translate('Size'), isSelected: (this.model.get('type') == 'Size')}
+      ]
+
+      return options
+    }
+
+  , getContext: function ()
+    {
+      return {
+        typeOptions: this.typeOptions()
+      }
+    }
+  })
+});
