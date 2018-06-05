b339eeb8206063599cefeb86596b0f0d3947ee6b
Deepak Bhari <deepakbhari@Deepaks-MBP-2.home>
2018-06-05 12:01:37 +0100

    Add a New Method to the Backend Model

--- Modules/extensions/UserPreferences@1.0.0/SuiteScript/UserPreferences.Model.js
+++ Modules/extensions/UserPreferences@1.0.0/SuiteScript/UserPreferences.Model.js
@@ -17,6 +17,18 @@ define('UserPreferences.Model'
         {internalid: 1, type: 'Color', value: '7'}
       , {internalid: 2, type: 'Size', value: '5'}
       ]
+    },
+
+    create: function (data)
+    {
+      var newRecord = nlapiCreateRecord('customrecord_user_preferences');
+
+      newRecord.setFieldValue('custrecord_user_preferences_owner', nlapiGetUser());
+      newRecord.setFieldValue('custrecord_user_preferences_type', data.type);
+      newRecord.setFieldValue('custrecord_user_preferences_value', data.value);
+
+      return nlapiSubmitRecord(newRecord);
     }
-  })
+
+  });
 });
