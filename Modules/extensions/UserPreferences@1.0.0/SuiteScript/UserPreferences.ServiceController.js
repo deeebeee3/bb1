ade960674d9dfe3615c644efe26372a01e0250f5
Deepak Bhari <deepakbhari@Deepaks-MBP-2.home>
2018-06-05 12:09:57 +0100

    Add a New Method to the Service Controller

--- Modules/extensions/UserPreferences@1.0.0/SuiteScript/UserPreferences.ServiceController.js
+++ Modules/extensions/UserPreferences@1.0.0/SuiteScript/UserPreferences.ServiceController.js
@@ -12,12 +12,21 @@ define('UserPreferences.ServiceController'
   'use strict';
 
   return ServiceController.extend({
-    name: 'UserPreferences.ServiceController'
-
-  , get: function ()
+    name: 'UserPreferences.ServiceController', 
+    
+    get: function ()
     {
       var id = this.request.getParameter('internalid');
       return id ? UserPreferencesModel.get(id) : UserPreferencesModel.list()
+    },
+
+    post: function()
+    {
+      /* Send content to the server using the create method of the UserPreferences model, 
+      with this data. Afterwards, return the 'created' HTTP code.*/
+      
+      this.sendContent(UserPreferencesModel.create(this.data), {'status': 201});
     }
-  })
+
+  });
 });
