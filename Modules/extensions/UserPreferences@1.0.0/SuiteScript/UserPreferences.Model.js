c2c6aa49dbf0a37ac97e5b78e3607a1c84b4cc00
Deepak Bhari <deepakbhari@Deepaks-MBP-2.home>
2018-06-05 13:42:05 +0100

    Get individual records - add GET method to backed model

--- Modules/extensions/UserPreferences@1.0.0/SuiteScript/UserPreferences.Model.js
+++ Modules/extensions/UserPreferences@1.0.0/SuiteScript/UserPreferences.Model.js
@@ -12,8 +12,7 @@ define('UserPreferences.Model'
   return SCModel.extend({
     name: 'UserPreferences', 
     
-    list: function ()
-    {
+    list: function () {
       /* 
       type: the internal ID of the record type we want to perform a search on
       id: the internal ID of the saved search (we're not using a saved search, so we can ignore this)
@@ -33,9 +32,11 @@ define('UserPreferences.Model'
       , new nlobjSearchColumn('custrecord_user_preferences_value')
       ];
 
-      /*Perform a search on user preferences records, filtering for ones that the current 
+      /*
+      Perform a search on user preferences records, filtering for ones that the current 
       user owns, returning to me only the internal ID, type (ie color or size) and value 
-      of that type (eg orange)*/
+      of that type (eg orange)
+      */
       
       var search = nlapiSearchRecord(type, null, filters, columns);
 
@@ -48,8 +49,7 @@ define('UserPreferences.Model'
       });
     },
 
-    create: function (data)
-    {
+    create: function (data) {
       var newRecord = nlapiCreateRecord('customrecord_user_preferences');
 
       newRecord.setFieldValue('custrecord_user_preferences_owner', nlapiGetUser());
@@ -57,6 +57,40 @@ define('UserPreferences.Model'
       newRecord.setFieldValue('custrecord_user_preferences_value', data.value);
 
       return nlapiSubmitRecord(newRecord);
+    },
+
+    get: function (id) {
+      var type = 'customrecord_user_preferences';
+
+      /*
+      Getting a single record rather than a list, requires/allows us to look up a record using 
+      its internal ID, which we specify as a filter
+      */
+      var filters = [
+        new nlobjSearchFilter('custrecord_user_preferences_owner', null, 'anyof', nlapiGetUser())
+      , new nlobjSearchFilter('internalid', null, 'is', id)
+      ];
+
+      var columns = [
+        new nlobjSearchColumn('internalid')
+      , new nlobjSearchColumn('custrecord_user_preferences_type')
+      , new nlobjSearchColumn('custrecord_user_preferences_value')
+      ];
+
+      var search = nlapiSearchRecord(type, null, filters, columns);
+
+      /*
+      Rather than map the results (ie produce an array of objects), we return a simple 
+      object of the fields we want
+      */
+      if (search && search.length === 1)
+      {
+        return {
+          internalid: search[0].getValue('internalid')
+        , type: search[0].getText('custrecord_user_preferences_type')
+        , value: search[0].getValue('custrecord_user_preferences_value')
+        }
+      }
     }
 
   });
