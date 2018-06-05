f887ea8de0da285c0290eb6ab4fbc34afa470dea
Deepak Bhari <deepakbhari@Deepaks-MBP-2.home>
2018-06-05 09:35:10 +0100

    Add a collection view as a child view

--- Modules/extensions/UserPreferences@1.0.0/JavaScript/UserPreferences.List.View.js
+++ Modules/extensions/UserPreferences@1.0.0/JavaScript/UserPreferences.List.View.js
@@ -2,17 +2,27 @@ define('UserPreferences.List.View'
 , [
     'Backbone'
   , 'user_preferences_list.tpl'
+  , 'Backbone.CollectionView'
+  , 'UserPreferences.Details.View'
   ]
 , function
   (
     Backbone
   , user_preferences_list_tpl
+  , CollectionView
+  , UserPreferencesDetailsView
   )
 {
   'use strict';
 
   return Backbone.View.extend({
     template: user_preferences_list_tpl, 
+
+    initialize: function (options)
+    {
+      this.application = options.application
+    , this.collection = options.collection
+    },
     
     /*A view/template's context is an object that contains all of the values that we want to 
     pass from a view to a template. They can be the results of complex functions, data values, 
@@ -23,6 +33,18 @@ define('UserPreferences.List.View'
       return {
         message: 'Hello world! 🌍👋'
       }
+    },
+
+    childViews:{
+      'UserPreferences.Collection': function ()
+      {
+        return new CollectionView({
+          'childView': UserPreferencesDetailsView
+        , 'collection': this.collection
+        , 'viewsPerRow': 1
+        })
+      }
     }
+
   })
 });
